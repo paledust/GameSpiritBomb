f7cbc1636fd7219c289eebbea29f4377b9de984d
Kevin <paledust@hotmail.com>
2020-02-16 19:03:18 +0800

    Remove Unused Package

--- Game Spirit Bomb-Unity/Assets/Scripts/GameManager.cs
+++ Game Spirit Bomb-Unity/Assets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Prefabs")]
     public GameObject GridPrefab;
     public GameObject VirusRedPrefab;
     public GameObject VirusGreyPrefab;
     public GameObject PlayerGreenPrefab;
+    [Header("Level")]
     public int Level=1;
     public int TotalGrids{get{return (Level+2)*(Level*2);}}
     public int RowCount{get{return Level+2;}}
@@ -15,6 +17,7 @@ public class GameManager : MonoBehaviour
     public int Steps{get; protected set;}
     public int HealthCount{get; protected set;}
     public static GameManager instance;
+    //设置GameManager为Singleton，且不会因为加载关卡被重置
     void Awake(){
         if(instance == null){
             instance = this;
@@ -24,16 +27,24 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    //重新加载关卡
     public void RefreshLevel(){
 
     }
+    //加载下一关
     public void NextLevel(){
         Level ++;
         RefreshLevel();
     }
+    //创建格子
     public void GenerateGrid(){
+        float width = GridPrefab.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        float height;
+        Vector2 pos;
+
         for(int i=0; i<TotalGrids; i++){
 
+            // GameObject grid = Instantiate()
         }
     }
 }
