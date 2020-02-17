e0cc611dcbb9e4c754cb1c262539862818ca77d2
yzxjason@live.cn <yzxjason@live.cn>
2020-02-17 20:29:43 +0800

    增加开始界面

---
+++ Game Spirit Bomb-Unity/Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    // 按钮
+    public Button startButton;
+
+    public Transform Runing;
+    public Transform NotDisplay;
+
+    // 显示 Text
+    public Text levelText;
+    public Text stepText;
+    public Text virusCountText;
+    public Text healthCountText;
+
+    [HideInInspector]
+    public int healthCount;
+    [HideInInspector]
+    public int virusCount;
+    [HideInInspector]
+    public int step;
+    [HideInInspector]
+    public int level;
+    private void Awake() {
+        Runing.parent = NotDisplay;
+        startButton.transform.parent.parent = transform;
+    }
+    private void Start() {
+
+
+        startButton.onClick.AddListener (StartGame);
+    }
+
+    // 开始按钮
+    void StartGame() {
+        Runing.parent = transform;
+        startButton.transform.parent.parent = NotDisplay;
+    }
+
+}
