7886ee1bf264fcd2a79b1283c30e72efb6e4384a
Kevin <paledust@hotmail.com>
2020-02-19 02:09:34 +0800

    更新感染值和健康值的数值标志

--- Game Spirit Bomb-Unity/Assets/Scripts/Interaction/Hover_Grid.cs
+++ Game Spirit Bomb-Unity/Assets/Scripts/Interaction/Hover_Grid.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class Hover_Grid : MonoBehaviour
 {
     public Color HighLightColor = Color.white;
+    public TextMesh text;
     protected Color originalColor;
-    protected float highLightFactor = 0;
-    protected float targetFactor = 1;
     void Start(){
         originalColor = GetComponent<SpriteRenderer>().color;
     }
     void OnMouseEnter(){
-        
     }
     void OnMouseExit(){
-        // targetFactor
     }
 }
