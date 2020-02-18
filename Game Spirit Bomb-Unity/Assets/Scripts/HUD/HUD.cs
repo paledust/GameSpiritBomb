4777ba889981bfe475b08e485e0af28a43f07a18
Niiga <yzxjason@live.cn>
2020-02-19 00:38:21 +0800

    push

--- Game Spirit Bomb-Unity/Assets/Scripts/HUD/HUD.cs
+++ Game Spirit Bomb-Unity/Assets/Scripts/HUD/HUD.cs
@@ -7,9 +7,13 @@ public class HUD : MonoBehaviour
 {
     // 按钮 
     public Button startButton;
+    public Button runingBackButton; // 运行中的返回按钮
+    public Button gameoverBackButton;   // 游戏结束界面的返回按钮
+    public Button gameoverNextLevelButton;   // 游戏结束界面的返回按钮
+    public Text gameoverText;
     // 界面
     public Transform Runing;
-    public Transform Win;
+
     // 声音
     public GameObject audioOBJ;
     // 显示 Text
@@ -18,30 +22,52 @@ public class HUD : MonoBehaviour
     public Text virusCountText;
     public Text healthCountText;
 
-    // 显示参数
-    public GameManager manager;
-
     private void Awake() {
-        audioOBJ.GetComponent<AudioManager> ().PlayAudio(0);
-        Runing.gameObject.SetActive (false);
-        startButton.transform.parent.gameObject.SetActive (true);
+        OnBackButton ();
     }
     private void Start() {
         startButton.onClick.AddListener (StartGame);
+        runingBackButton.onClick.AddListener (OnBackButton);
+        gameoverBackButton.onClick.AddListener (OnBackButton);
+        gameoverNextLevelButton.onClick.AddListener (NextLevel);
     }
 
     private void Update() {
-        levelText.text = "level:" + manager.Level;
-        healthCountText.text = "HealthCount:" + manager.HealthCount;
-        virusCountText.text = "VirusCount:" + manager.virusCount;
-        stepText.text = "Step:" + manager.Steps;
+        levelText.text = "level:" + GameManager.instance.Level;
+        healthCountText.text = "HealthCount:" + GameManager.instance.HealthCount;
+        virusCountText.text = "VirusCount:" + GameManager.instance.virusCount;
+        stepText.text = "Step:" + GameManager.instance.Steps;
     }
-
+    // 返回按钮
+    void OnBackButton() {
+        Runing.gameObject.SetActive (false);
+        startButton.transform.parent.gameObject.SetActive (true);
+        gameoverText.transform.parent.gameObject.SetActive (false);
+        audioOBJ.GetComponent<AudioManager> ().PlayAudio (0);
+    }
     // 开始按钮
     void StartGame() {
-        Runing.gameObject.SetActive (true);
         startButton.transform.parent.gameObject.SetActive (false);
+        Runing.gameObject.SetActive (true);
         audioOBJ.GetComponent<AudioManager> ().PlayAudio (1);
+    }
+
+    public void DisplayWinScenes() {
+        gameoverText.transform.parent.gameObject.SetActive (true);
+        gameoverText.text = "游戏胜利";
+        gameoverNextLevelButton.interactable = true;
+        audioOBJ.GetComponent<AudioManager> ().PlaySound (6); // 音效 success
+    }
+
+    public void DisplayLoseScenes() {
+        gameoverText.transform.parent.gameObject.SetActive (true);
+        gameoverText.text = "游戏失败";
+        gameoverNextLevelButton.interactable = false;
+        audioOBJ.GetComponent<AudioManager> ().PlaySound (7); // 音效 fail
+    }
+
+    void NextLevel() {
+        GameManager.instance.NextLevel ();
     }
 
 }
