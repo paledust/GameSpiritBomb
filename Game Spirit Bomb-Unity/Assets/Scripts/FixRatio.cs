62ed15250078d15ed44c37697cbe5452a679f0a5
Kevin <paledust@hotmail.com>
2020-02-19 11:32:17 +0800

    固定屏幕分辨率

---
+++ Game Spirit Bomb-Unity/Assets/Scripts/FixRatio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FixRatio : MonoBehaviour {
+	protected Camera AspectCamera;
+	public Vector2Int ReferenceRatio = new Vector2Int(16,9);
+	// Use this for initialization
+	void Update () {
+		AspectCamera = GetComponent<Camera>();
+
+		float x = 1;
+		if(Screen.width/ReferenceRatio.x != Screen.height/ReferenceRatio.y){
+			x = ((float)Screen.height/(float)Screen.width)/((float)ReferenceRatio.y/(float)ReferenceRatio.x);
+		}
+
+		AspectCamera.rect = new Rect((1-x)/2,0,x,1);
+	}
+}
