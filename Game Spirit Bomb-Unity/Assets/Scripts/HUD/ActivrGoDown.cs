bcf2433fa13973ba56c4d4b75789622afe2892b0
Niiga <yzxjason@live.cn>
2020-02-20 16:53:15 +0800

    完成结束界面动态图片设置

--- Game Spirit Bomb-Unity/Assets/Scripts/HUD/ActivrGoDown.cs
+++ Game Spirit Bomb-Unity/Assets/Scripts/HUD/ActivrGoDown.cs
@@ -8,22 +8,74 @@ public class ActivrGoDown : MonoBehaviour
     public Sprite WinImage;
     public Sprite LoseImage;
     public HUD hud;
-    RectTransform rect;
-    public bool isActivr = false;
-    float speed = 2f;
-    float curSpeed;
+    RectTransform hudRect;
+    RectTransform rectTransform;
+    bool isActivr = false;
+    public bool isTime = false;
+    float g = 6f;
+    float curSpeed = 2f;
+    float time;
+    int[] jumpHeights;
+    int num = 0;
     // Update is called once per frame
     private void Awake() {
-        rect = GetComponent<RectTransform> ();
+        hudRect = hud.gameObject.GetComponent<RectTransform> ();
+        rectTransform = GetComponent<RectTransform> ();
     }
+
+    private void Start() {
+        jumpHeights = new int[] { 
+            (int)(hud.gameoverImage.rectTransform.rect.height * 2f),
+            (int)(hud.gameoverImage.rectTransform.rect.height * 1.5f), 
+            (int)hud.gameoverImage.rectTransform.rect.height};
+    }
+    bool isDown = true;
+    bool isUp = false;
     void Update()
     {
+        if (isTime) {
+            isTime = false;
+            time = Time.time;
+            isActivr = true;
+        }
         if (isActivr) {
-
+            rectTransform.localPosition = new Vector3 (rectTransform.localPosition.x,
+                rectTransform.localPosition.y - g * (Time.time - time) * curSpeed );
+            if (rectTransform.localPosition.y <= hud.gameoverImage.rectTransform.rect.height && isDown) {
+                Debug.Log ($"调用 y < image.height");
+                g = -(Mathf.Abs (g) - 1.6f);
+                //time = Time.time;
+                isDown = false;
+                isUp = true;
+            }
+            if (rectTransform.localPosition.y >= (float)jumpHeights[num] && isUp) {
+                Debug.Log ($"调用 jumpHeights[]");
+                g = (Mathf.Abs (g) - 0.6f);
+                num++;
+                if (num == jumpHeights.Length) {
+                    isActivr = false;
+                }
+                isUp = false;
+                isDown = true;
+            }
+            
         }
     }
 
-    void GoDown() {
+    public void ChangeImage(Sprite _img) {
+        this.GetComponent<Image> ().sprite = _img;
+    }
+    // 重置关卡跳跃
+    public void ResetActivr() {
         
+        isDown = true;
+        isUp = false;
+        isTime = false;
+        isActivr = false;
+        g = 6f;
+        num = 0;
+        time = Time.time;
+        rectTransform.localPosition = new Vector3 (0,
+            hudRect.rect.height);
     }
 }
