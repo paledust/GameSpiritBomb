4413ebb2696bfba7d99faed92442fa5ed10bf9bf
Niiga <yzxjason@live.cn>
2020-02-22 20:43:48 +0800

    增加退出按钮

--- Game Spirit Bomb-Unity/Assets/Scripts/HUD/HUD.cs
+++ Game Spirit Bomb-Unity/Assets/Scripts/HUD/HUD.cs
@@ -10,6 +10,10 @@ public class HUD : MonoBehaviour
     public Button runingBackButton;     // 运行中的返回按钮
     public Button gameoverWinButton;    // 游戏结束界面按钮
     public Button gameoverLoseButton;   // 游戏结束界面按钮
+    public Button ExitButton;           // 开始界面退出按钮
+    public Button ExitOKButton;     // 退出界面确定按钮
+    public Button ExitCancelButton; // 退出界面取消按钮
+
     public Button staffButton;          // 制作组名单按钮
     public GameObject staffOBJ;         // 制作组名单界面
     public Image gameoverImage;         // 游戏结束界面图片
@@ -18,6 +22,7 @@ public class HUD : MonoBehaviour
     public Button OFFhowToPlayButton;  // 打开 玩法介绍按钮
     public Button ONhowToPlayButton;   // 关闭 玩法介绍按钮
     public GameObject howToPlayOBJ;   // 关闭 玩法介绍按钮
+
 
     float staffCD = 5f;
     float staffTime = 0f;
@@ -37,26 +42,40 @@ public class HUD : MonoBehaviour
     public Sprite LoseImg;
 
     private void Awake() {
-        //Screen.SetResolution (607, 1080, false); // PC打包默认大小
         OnBackButton ();
 
     }
     private void Start() {
+        // 开始游戏 按钮
         startButton.onClick.AddListener (StartGame);
+        // 运行界面的返回按钮
         runingBackButton.onClick.AddListener (OnBackButton);
+        // 显示制作组名单按钮
         staffButton.onClick.AddListener (DisplayStaff);
+        // 游戏结束 胜利界面按钮
         gameoverWinButton.onClick.AddListener (NextLevel);
+        // 游戏结束 失败界面按钮
         gameoverLoseButton.onClick.AddListener (RefreshLevel);
+        // 显示游戏玩法按钮
         OFFhowToPlayButton.onClick.AddListener (DispalyHowToPlay);
+        // 关闭游戏玩法按钮
         ONhowToPlayButton.onClick.AddListener (DispalyHowToPlay);
+        // 打开退出界面按钮
+        ExitButton.onClick.AddListener (DisplayQuitOrCancelScene);
+        // 退出游戏按钮
+        ExitOKButton.onClick.AddListener (QuitSceneOKButton);
+        // 取消退出游戏
+        ExitCancelButton.onClick.AddListener (DisplayQuitOrCancelScene);
     }
 
     private void Update() {
+        // 界面文字显示
         levelText.text = "level:" + GameManager.instance.Level;
         healthCountText.text = "HealthCount:" + GameManager.instance.HealthCount;
         virusCountText.text = "VirusCount:" + GameManager.instance.virusCount;
         stepText.text = "Step:" + GameManager.instance.Steps;
 
+        // 制作组名单自动关闭
         if (Time.time - staffCD > staffTime && isSetStaff == true) {
             staffOBJ.SetActive (false);
             isSetStaff = false;
@@ -64,25 +83,36 @@ public class HUD : MonoBehaviour
 
     }
 
+    // 按 退出按钮
+    void DisplayQuitOrCancelScene() {
+        ExitOKButton.transform.parent.gameObject.SetActive (!ExitOKButton.transform.parent.gameObject.activeSelf);
+    }
+
+    void QuitSceneOKButton() {
+        Application.Quit ();
+    }
+
+    // 按 显示or关闭 游戏玩法介绍按钮
     void DispalyHowToPlay() {
         Debug.Log ($"{howToPlayOBJ.activeSelf}");
         howToPlayOBJ.SetActive (!howToPlayOBJ.activeSelf); // 默认关闭
     }
 
-    // 返回按钮
+    // 按 返回按钮
     void OnBackButton() {
         Running.gameObject.SetActive (false);
         startButton.transform.parent.gameObject.SetActive (true);
         gameoverLoseButton.transform.parent.gameObject.SetActive (false);
         audioOBJ.GetComponent<AudioManager> ().PlayAudio (0);
     }
-    // 开始按钮
+    // 按 开始按钮
     void StartGame() {
         startButton.transform.parent.gameObject.SetActive (false);
         Running.gameObject.SetActive (true);
         audioOBJ.GetComponent<AudioManager> ().PlayAudio (1);
     }
 
+    // 胜利界面
     public void DisplayWinScenes() {
         gameoverImage.transform.parent.gameObject.SetActive (true);
         gameoverImage.sprite = WinImg;
@@ -91,6 +121,7 @@ public class HUD : MonoBehaviour
         audioOBJ.GetComponent<AudioManager> ().PlaySound (6); // 音效 success
         StartCoroutine ("IWinDisplay");
     }
+    // 失败界面
     public void DisplayLoseScenes() {
         gameoverImage.transform.parent.gameObject.SetActive (true);
         gameoverImage.sprite = LoseImg;
@@ -99,14 +130,15 @@ public class HUD : MonoBehaviour
         audioOBJ.GetComponent<AudioManager> ().PlaySound (7); // 音效 fail
         StartCoroutine ("ILoseDisplay");
     }
-
-    IEnumerator ILoseDisplay() {
-        yield return 0.1f;
-        gameoverLoseButton.gameObject.SetActive (true);
-    }
+    // 延迟显示胜利界面按钮
     IEnumerator IWinDisplay() {
         yield return 0.1f;
         gameoverWinButton.gameObject.SetActive (true);
+    }
+    // 延迟显示失败界面按钮
+    IEnumerator ILoseDisplay() {
+        yield return 0.1f;
+        gameoverLoseButton.gameObject.SetActive (true);
     }
 
     // 进行下一关
@@ -118,6 +150,7 @@ public class HUD : MonoBehaviour
         goDownScript.ResetActivr ();
     }
 
+    // 重玩本关
     void RefreshLevel() {
         gameoverWinButton.gameObject.SetActive (false);
         gameoverLoseButton.gameObject.SetActive (false);
@@ -126,6 +159,7 @@ public class HUD : MonoBehaviour
         goDownScript.ResetActivr ();
     }
 
+    // 按 显示制作组名单按钮
     void DisplayStaff() {
         staffTime = Time.time;
         isSetStaff = true;
